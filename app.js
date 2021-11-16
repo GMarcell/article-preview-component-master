131f4e2777f34e99c02c4ff4e7acfde6856b4a12
GMarcell <grand1310marcell@gmail.com>
2021-11-16 08:49:05 +0700

    Done

--- app.js
+++ app.js
@@ -8,10 +8,26 @@ function sharefunction() {
 }
 
 function profilefunction() {
-    if (document.getElementById("profile").classList.contains("active")) {
-        document.getElementById("profile").classList.remove("active");
-        document.getElementById("sharesection").classList.remove("hide");
+    if (screen.width >= 1023) {
+        console.log(screen.width)
+        if (document.getElementById("sharesection").style.display == "none") {
+            document.getElementById("sharesection").style.display = "grid";
+        } else {
+            document.getElementById("sharesection").style.display = "none"
+        }
+        // if (document.getElementById("sharesection").classList.contains("active")) {
+        //     document.getElementById("sharesection").classList.remove("active");
+        //     document.getElementById("sharesection").classList.toggle("hide");
+        // } else {
+        //     document.getElementById("sharesection").classList.toggle("active")
+        //     document.getElementById("sharesection").classList.remove("hide");
+        // }
+    } else {
+        if (document.getElementById("profile").classList.contains("active")) {
+            document.getElementById("profile").classList.remove("active");
+            document.getElementById("sharesection").classList.remove("hide");
+        }
+        document.getElementById("profile").classList.toggle("hide")
+        document.getElementById("sharesection").classList.toggle("active");
     }
-    document.getElementById("profile").classList.toggle("hide")
-    document.getElementById("sharesection").classList.toggle("active");
 }
